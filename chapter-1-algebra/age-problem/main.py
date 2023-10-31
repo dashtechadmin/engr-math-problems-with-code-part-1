fb782325389bad119c1d72accb1f60c130631985
Mark Silverio <markjuliusb.silveriobiz@gmail.com>
2023-10-31 02:00:04 +0000

    feat: added chapter-1-algebra::age-problem

---
+++ chapter-1-algebra/age-problem/main.py
@@ -0,0 +1,33 @@
+class Solution:
+
+    def solution1(self):
+        '''
+        How many more years so that son and father have 2 as their age ratio?
+        (f+x)/(s+x) = 2, in x years
+        f + x = 2s + 2x
+        f - 2s = x or x = f - 2s
+        f - rs or f - 2s
+        '''
+        f = 31
+        s = 1
+        r = 2
+        return f - r * s # by that time, son is 30 years old, father is 60 years old
+
+
+
+    # def solution2(self):
+        
+
+    def run(self):
+        print(self.solution1())
+
+        # print(self.solution2())
+
+sol = Solution()
+sol.run()
+
+"""
+PROBLEMS:
+1. A man 42 years old has a son 12. In how many years will the father be twice as old as his son?
+
+"""
--- commit.sh
+++ commit.sh
@@ -1,3 +1,3 @@
 git add .
-git commit -m "feat: added chapter-1-algebra"
+git commit -m "feat: added chapter-1-algebra::age-problem"
 git push
